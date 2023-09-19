f35a73be23cf1776034d72efd337207b70447207
SteveDrees1 <Steve.drees@live.com>
2023-09-19 08:43:21 -0400

    Adding initial background and nav bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import MenuIcon from '@mui/icons-material/Menu';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <div>
+                <h1>
+                    <span className="fire">S</span>
+                    <span className="burn">T</span>
+                    <span className="burn">E</span>
+                    <span className="burn">V</span>
+                    <span className="fire">E</span>
+                    <span>&nbsp;</span>
+                    <span className="fire">D</span>
+                    <span className="burn">R</span>
+                    <span className="burn">E</span>
+                    <span className="burn">E</span>
+                    <span className="fire">S</span>
+
+                </h1>
+                <nav>
+                    <MenuIcon/>
+                    <AccountBoxIcon className={"accountIcon"}/>
+                </nav>
+            </div>
+            <div>
+                <div className="stars"></div>
+                <div className="twinkling"></div>
+                <div className="clouds"></div>
+            </div>
+        </div>
+    )
 }
 
 export default App;
